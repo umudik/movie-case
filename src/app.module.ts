4f200927d9c63b02bdb171e3d66a8e1cdfd0c46f
umudik <seyitumutbicer@gmail.com>
2023-05-20 12:55:52 +0300

    feat!: add auth service.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
-import { UserModule } from './user/user.module';
+import { AuthController } from './auth/auth.controller';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { AuthService } from './auth/auth.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
-  imports: [UserModule],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  controllers: [AppController, AuthController, UserController],
+  providers: [AppService, JwtService, PrismaService, UserService, AuthService],
 })
 export class AppModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,9 @@
+// src/auth/auth.service.ts
 import { Injectable } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
 import { UserService } from '../user/user.service';
+import { JwtService } from '@nestjs/jwt';
+import { User, Prisma } from '@prisma/client';
+import { createHmac } from 'crypto';
 
 @Injectable()
 export class AuthService {
@@ -9,19 +12,23 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
+  hashPassword(password: string): string {
+    return createHmac('sha256', password).digest('hex');
+  }
+
   async validateUser(email: string, password: string): Promise<any> {
     const user = (await this.userService.find({ email }))[0];
-
-    if (user && user.password === password) {
-      return user;
+    if (user && user.password === this.hashPassword(password)) {
+      const { password, ...result } = user;
+      return result;
     }
     return null;
   }
 
-  async login(user: any) {
-    const payload = { username: user.username, sub: user.userId };
+  async login(user: User) {
+    const payload = { id: user.id };
     return {
-      access_token: this.jwtService.sign(payload),
+      token: this.jwtService.sign(payload),
     };
   }
 }
