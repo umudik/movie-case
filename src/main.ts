ba2ede129e12745d59959238e6951e83c935fce1
umudik <seyitumutbicer@gmail.com>
2023-05-23 01:19:28 +0300

    feat: port

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
